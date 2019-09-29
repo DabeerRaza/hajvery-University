83664204d65863a213f378b626e0abb053e800dd
Dabeer <dabeerofficial@gmail.com>
2019-09-29 19:03:09 +0500

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$hostAddress = "localhost";
+$userName = "root";
+$password = "";
+$dataBase = "hu_update_via_email";
+
+$conn=new mysqli($hostAddress,$userName,$password,$dataBase) or die("error in connection")
+
+?>
